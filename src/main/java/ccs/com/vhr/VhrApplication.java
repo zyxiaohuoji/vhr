5508b82ed031b221efd68cb93d48e9f708793dfd
zyxiaohuoji <1275819010@qq.com>
2020-11-04 11:58:56 +0800

    first commit

---
+++ src/main/java/ccs/com/vhr/VhrApplication.java
@@ -0,0 +1,13 @@
+package ccs.com.vhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrApplication.class, args);
+    }
+
+}
